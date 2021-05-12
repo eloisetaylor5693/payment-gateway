a5d57019e99f196c12b4f32b554a0531f6d3babe
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-05-12 09:44:24 +0100

    added draft spes for /make-a-payment

---
+++ e2e/cypress/integration/make-a-payment.spec.js
@@ -0,0 +1,13 @@
+describe('api/payment/make-a-payment', () => {
+    describe('When making a valid payment request', () => {
+        it('Then it succeeds', () => { });
+    });
+
+    describe('When making an invalid payment request', () => {
+        it('then payment is rejected', () => { });
+    });
+
+    describe('when I repeat the same payment', () => {
+        it('then payment is rejected', () => { });
+    });
+});
