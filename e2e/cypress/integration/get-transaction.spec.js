8b745fd9955eb00c6a3954d124975ed5b5174d73
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-05-12 09:48:35 +0100

    added draft spec for /get-transaction

---
+++ e2e/cypress/integration/get-transaction.spec.js
@@ -0,0 +1,9 @@
+describe('api/payment/get-transaction', () => {
+    describe('Given a payment ID that exists', () => {
+        it('Then payment information is returned', () => { });
+    });
+
+    describe("Given a payment ID that doesn't exist", () => {
+        it('then response advises the ID is not found', () => { });
+    });
+});
