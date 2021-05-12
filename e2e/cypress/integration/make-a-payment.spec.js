9200d6a8350a1ae3070d3194ec686f0af80151ce
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-05-12 17:56:57 +0100

    Implemented example payment request in cypress test

--- e2e/cypress/integration/make-a-payment.spec.js
+++ e2e/cypress/integration/make-a-payment.spec.js
@@ -1,6 +1,27 @@
 describe('api/payment/make-a-payment', () => {
     describe('When making a valid payment request', () => {
-        it('Then it succeeds', () => { });
+        it('Then it succeeds', () => {
+            const request = {
+                "transactionDate": "2021-05-12T16:26:36.246Z",
+                "merchantId": 123,
+                "terminalId": 5,
+                "paymentAmount": 32.93,
+                "isoCurrencyCode": "GBP",
+                "paymentReference": "Order #2325",
+                "card": {
+                    "nameOnCard": "Miss Anne Other",
+                    "cardIssuer": "Mastercard",
+                    "cardNumber": "5574123412341234",
+                    "expiryDate": "05/55",
+                    "cvv": 123
+                }
+            };
+
+            cy.request('POST', '/make-a-payment', request)
+                .then(response => {
+                    expect(response.status).to.equal(200);
+                })
+        });
     });
 
     describe('When making an invalid payment request', () => {
