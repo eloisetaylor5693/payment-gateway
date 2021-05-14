43aa369a2bf458378760c3a316784418f7655b11
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-05-14 09:03:12 +0100

    Validating the card number

---
+++ src/PaymentGateway.UnitTests/CardValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using NUnit.Framework;
+using PaymentGateway.Models;
+using PaymentGateway.Validation;
+
+namespace PaymentGateway.UnitTests
+{
+    [TestFixture]
+    public sealed class CardValidatorTests
+    {
+        [Test]
+        public void PassesValidation()
+        {
+            var validator = new CardValidator();
+
+            validator.ValidateAndThrow(ValidCard);
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("1")]
+        [TestCase("1234123412341234")]
+        [TestCase("1234 1234 1234 1234")]
+        [Test]
+        public void WhenInvalidCardNumber_FailsValidation(string invalidCardNumber)
+        {
+            var invalidRequest = ValidCard;
+            invalidRequest.CardNumber = invalidCardNumber;
+
+            var validator = new CardValidator();
+
+            validator.ValidateAndThrow(ValidCard);
+        }
+
+        private Card ValidCard => new Card
+        {
+            NameOnCard = "Miss Anne Other",
+            CardNumber = "6331101999990016",
+            ExpiryDate = "05/25",
+            CVV = 123
+        };
+    }
+}
---
+++ src/PaymentGateway/Validation/CardValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using PaymentGateway.Models;
+
+namespace PaymentGateway.Validation
+{
+    public class CardValidator : AbstractValidator<Card>
+    {
+        public CardValidator()
+        {
+            RuleFor(x => x.CardNumber)
+                .NotNull()
+                .CreditCard();
+        }
+    }
+}
