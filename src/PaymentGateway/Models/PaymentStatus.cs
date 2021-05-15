7b0ee914095ff60be1c2c98968ebc2c73a947002
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-05-15 11:23:07 +0100

    Added enum with different payment status codes

---
+++ src/PaymentGateway/Models/PaymentStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace PaymentGateway.Models
+{
+    public enum PaymentStatus
+    {
+        Unknown = 0,
+        Success = 1,
+        NotEnoughFunds = 2,
+        CardFrozen = 3,
+        SuspiciousActivity = 4,
+
+        // Leaving space for other failure statuses before this one
+        DeclinedForOtherReason = 20
+    }
+}
