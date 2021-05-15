d73f55a3c33bb4186e10f63247b1fd45dae552a5
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-05-15 11:23:19 +0100

    Added paymentTransaction object for persisting

---
+++ src/PaymentGateway/Models/PaymentTransaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PaymentGateway.Models
+{
+    public class PaymentTransaction
+    {
+        public Guid TransationId { get; set; }
+
+        public Guid BankTransactionId { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+
+        public string MerchantId { get; set; }
+
+        public string TerminalId { get; set; }
+
+        public double PaymentAmount { get; set; }
+
+        public string IsoCurrencyCode { get; set; }
+
+        public string PaymentReference { get; set; }
+
+        public Card Card { get; set; }
+
+        public bool TransactionSucessful { get; set; }
+
+        public PaymentStatus PaymentStatus { get; set; }
+    }
+}
